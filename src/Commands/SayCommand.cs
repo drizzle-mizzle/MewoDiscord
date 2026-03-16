d0baa76896910b1c4eec8ac32f61383e5449baf7
drizzle-mizzle <drizzle-mizzle@hotmail.com>
2026-03-16 05:11:35 +0500

    say

---
+++ src/Commands/SayCommand.cs
@@ -0,0 +1,19 @@
+using Discord;
+using Discord.Interactions;
+using MewoDiscord.Helpers;
+
+namespace MewoDiscord.Commands;
+
+[DefaultMemberPermissions(GuildPermission.Administrator)]
+public class SayCommand : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("say", "Написать сообщение от имени бота")]
+    public async Task Say(
+        [Summary("text", "Текст сообщения")]
+        string text)
+    {
+        await Context.Channel.SendMessageAsync(text);
+        BotLogger.Information("{User} использовал /say в #{Channel}", Context.User.Username, Context.Channel.Name);
+        await RespondAsync("✅", ephemeral: true);
+    }
+}
